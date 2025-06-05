d7834f88b4f01e2e6cebb97f9973210d5ebd86b4
anshhh29 <anshpopli29@gmail.com>
2025-06-05 11:46:07 +0530

    loop ai

--- server.js
+++ server.js
@@ -2,10 +2,7 @@ const app = require('./app');
 const mongoose = require('mongoose');
 require('dotenv').config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
-}).then(() => {
+mongoose.connect(process.env.MONGO_URI).then(() => {
   console.log('MongoDB connected');
 
   if (process.env.NODE_ENV !== 'test') {
